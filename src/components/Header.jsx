05b424f0cd9cf6bd736e7540fb49ea0af21388b1
phildan02 <phildan02@gmail.com>
2023-10-21 12:08:26 +0300

    'first commit'

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/VK_Compact_Logo_%282021-present%29.svg/2048px-VK_Compact_Logo_%282021-present%29.svg.png' />
+        </header>
+    );
+};
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src='https://n1s1.hsmedia.ru/7d/f4/65/7df465013dd467c13ab94d0ff1c2cd4d/1000x600_0xac120003_8887241501666366066.jpeg' alt=''/>
+            </div>
+            <div>
+                avatar + description
+            </div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    <div>
+                        post1
+                    </div>
+                    <div>
+                        post2
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Profile;
